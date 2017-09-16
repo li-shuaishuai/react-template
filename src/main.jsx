e805f604070456ef952d70df68576456dc195992
lishuaishuai <lishuaishuai_it@163.com>
2017-09-16 18:19:13 +0800

    脚手架初始化

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Hello from 'components/hello';
+
+ReactDom.render(
+    <Hello />,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/main.jsx'), //入口文件
+  output: {
+    path: path.resolve(__dirname, './build'),//打包后的文件存放的地方
+    filename: "bundle.js"//打包后输出文件的文件名
+  },
+  devtool: 'eval-source-map',//生成Source Maps  
+  devServer: {
+    contentBase: "./build",//本地服务器所加载的页面所在的目录
+    historyApiFallback: true,//不跳转
+    inline: true,//实时刷新
+    hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      components: path.resolve(__dirname, 'src/components/')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }, {
+            loader: "postcss-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin('Created by Li Shuaishuai GitHub:https://github.com/li-shuaishuai'),
+    // html 模板插件
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.tmpl.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()//热加载插件
+  ],
+};
