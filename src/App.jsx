3f0d49cd6edc97a7a6c1842f100c34dd9e186c8d
lishuaishuai <lishuaishuai.it@gmail.com>
2018-11-20 16:51:58 +0800

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+{{#if router}}
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+{{/if}}
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './views/home'
@@ -13,20 +15,21 @@ class App extends React.Component {
       {{#if router}}
       <BrowserRouter>
         <React.Fragment>
-      {{/if}}
           <Header />
-          {{#if router}}
           <Switch>
             <Route exact path="/" component={Home}></Route>
             <Route path="/about" component={About}></Route>
           </Switch>
-          {{else}}
           <Home/>
-          {{/if}}
           <Footer />
-      {{#if router}}
         </React.Fragment>
       </BrowserRouter>
+      {{else}}
+      <React.Fragment>
+        <Header />
+        <Home/>
+        <Footer />
+      </React.Fragment>
       {{/if}}
     )
   }
