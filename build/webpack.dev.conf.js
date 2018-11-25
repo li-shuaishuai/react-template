838984cd8e78f54fc549467f03c242b5eb02d1c8
lishuaishuai <lishuaishuai.it@gmail.com>
2018-11-25 14:18:07 +0800

    remove webpack-dashboard

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,7 +2,6 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const baseWebpackConfig = require('./webpack.base.conf.js')
-const DashboardPlugin = require('webpack-dashboard/plugin')
 const config = require('../config')
 
 module.exports = merge(baseWebpackConfig, {
@@ -18,7 +17,6 @@ module.exports = merge(baseWebpackConfig, {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new DashboardPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.tpl.html'
     })
