7823913ade9544e938b5b32064beff89d1bf569c
lishuaishuai <lishuaishuai.it@gmail.com>
2019-01-23 16:39:06 +0800

    Update webpack config

--- config/index.js
+++ config/index.js
@@ -19,16 +19,14 @@ module.exports = {
   dev: {
     mode: NODE_ENV,
     sourceMap: 'source-map',
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     host: '0.0.0.0',
     port: 3001,
     autoOpenBrowser: true,
     overlay: true,
     historyApiFallback: true,
     noInfo: true
-  },
-  common: {
-    {{#if mobile}}
-    remUnit: 75 // 设计稿的宽度 在css及预编译语言中px自动转换为rem (https://www.npmjs.com/package/postcss-adaptive)
-    {{/if}}
   }
 }
